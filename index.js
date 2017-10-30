7f97f93e427fd3582f0e87c3bf496f8b5b9f8be3
Cris <c.caratti@me.com>
2017-10-30 17:24:58 +0000

    created a new website and woking on the header

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('I\'m the only js into this website! weird isn\' t it?');
+
+console.log("|  |  ||||  |     |     |||||");
+console.log("|  |  |     |     |     |   |");
+console.log("||||  ||||  |     |     |   |");
+console.log("|  |  |     |     |     |   |");
+console.log("|  |  ||||  ||||  ||||  |||||");
+
+
+
