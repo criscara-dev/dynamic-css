1a1cad383d56e2010adc2aed869e59319310aa4e
Cris <c.caratti@me.com>
2017-11-01 18:21:50 +0000

    working on[F animation button

--- index.js
+++ index.js
@@ -1,10 +1,55 @@
-console.log('I\'m the only js into this website! weird isn\' t it?');
 
-console.log("|  |  ||||  |     |     |||||");
+
+/*console.log("|  |  ||||  |     |     |||||");
 console.log("|  |  |     |     |     |   |");
 console.log("||||  ||||  |     |     |   |");
 console.log("|  |  |     |     |     |   |");
-console.log("|  |  ||||  ||||  ||||  |||||");
+console.log("|  |  ||||  ||||  ||||  |||||");*/
 
 
+console.log("|||    ||  ||||  |  |   |    ||   ||    || ");
+console.log("|  |  |  |    |  |  ||  |   |    |  |   || ");
+console.log("||||  ||||   |   |  | | |  |     ||||   || ");
+console.log("|  |  |  |  |    |  |  ||  |  |  |  |      ");
+console.log("|||   |  | ||||  |  |   |  ||||  |  |   || ");
+
+console.log('I\'m the only js into this website! weird isn\' t it?');
+
+
+console.log("                |||||||||||| ");
+console.log("            |||||||||||||||||||| ");
+console.log("         ||||||||||||||||||||||||");
+console.log("      ||||||||||||||||||||||||||||| ");
+console.log("     ||||||||||||||||||||||      |||||| ");
+console.log("   ||||        ||||||||            |||||| ");
+console.log("  |||                                |||||");
+console.log("  |||                                  |||");
+console.log("  ||                                  ||||| ");
+console.log("  ||                                  ||||| ");
+console.log("  ||                                  ||||| ");
+console.log("  ||                                  ||||| ");
+console.log(" ||                                     |||| ");
+console.log(" |||||||||                  |||||          || ");
+console.log("|  ||||||||||           |||||||||||       | ");
+console.log(" | |||||    |||||    |||||| |||  ||      ||| ");
+console.log(" |  ||           |         |___|           |||| ");
+console.log(" |   ||          |                       ||||| ");
+console.log("  |   ||        ||                       ||||| ");;
+console.log("  |   |        ||                        || ");
+console.log("   |  ||       |       |                || ");
+console.log("   |  ||        |||||||               | ");
+console.log("   |  |||        ||||                | ");
+console.log("   ||                                | ");
+console.log("     || ||||||            ||        | ");
+console.log("      | |    |||||||||||||        | ");
+console.log("       | |    |                  || ");
+console.log("        |       |||||||        | ");
+console.log("          |||                || ");
+console.log("             |||           || ");
+console.log("               |          || ");
+console.log("                ||||||||||| ");
+
+
+
+console.log("OOOOOOOOOOOOOOOOOOOOOO");
 
